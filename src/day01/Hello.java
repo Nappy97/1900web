1d16dcc311334d60f2e75e9c516d39bd52cd7cf5
Nappy <wjddmlgud829@gmail.com>
2022-06-09 21:38:46 +0900

    Initial commit

---
+++ src/day01/Hello.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+
+        System.out.println("안녕하세요");
+        System.out.println("자바");
+    }
+}
